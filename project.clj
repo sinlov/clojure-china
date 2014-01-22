d812f7fa8b97ac06361ca7be5784d2032430f9cb
xhh <xhh@xhh.me>
2014-01-22 18:19:51 +0800

    Add react.js and director.js for development

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-china "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Source of the forum: clojure-china.org"
+  :url "http://clojure-china.org"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
@@ -12,11 +12,11 @@
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "clojure-china"
               :source-paths ["src"]
               :compiler {
-                :output-to "clojure_china.js"
+                :output-to "js/clojure_china.js"
                 :output-dir "out"
                 :optimizations :none
                 :source-map true}}]})
